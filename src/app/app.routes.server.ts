6fda2f0a2ddaf1fd8fca2c43056ee7c0573fed9f
flobatard <fb.batard@gmail.com>
2026-03-07 18:35:20 +0100

    Small improvments

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: "welcome",
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Client
   }
