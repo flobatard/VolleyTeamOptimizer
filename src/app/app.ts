28aea07b33bce762eeedcda469123458be8d1ae0
flobatard <fb.batard@gmail.com>
2026-03-08 12:44:42 +0100

    Better responsibe players data

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('VolleyTeamOptimizer');
+  protected readonly title = signal('Volley Team Optimizer');
 }
